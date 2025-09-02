5cc20b7131e822d8f2e8c5f4eacd8087557bd9fd
XCarrel <Xavier.Carrel@eduvaud.ch>
2025-09-02 10:13:25 +0200

    feat(htmljs): Add exceptions to the journal
    Journal entries not linked to a commit, kept in a separate file

--- htmljs/exceptions.js
+++ htmljs/exceptions.js
@@ -1,53 +1,38 @@
 const exceptions = [
   {
-    commit: {
-      patch: true,
-      message: "feat(htmlj): Add exception list\n\n[111][WIP]",
-      author: {
-        name: "XCarrel",
-        date: "2025-09-01T13:43:53Z"
-      }
-    }
+    patch: true,
+    name: "2025-09-01",
+    description: "It was bad",
+    date: "2025-09-01T13:43:53Z",
+    duration: 20,
+    status: "Done",
+    author: "GHerreraE"
   },
   {
-    commit: {
-      patch: true,
-      message: "feat(htmlj): Sub exception list\n\n[11][WIP]\nbla bla bla",
-      author: {
-        name: "XCarrel",
-        date: "2025-09-01T13:43:53Z"
-      }
-    }
+    patch: true,
+    name: "2025-03-10",
+    description: "It was not doing much",
+    date: "2025-03-10T13:43:53Z",
+    duration: 120,
+    status: "Done",
+    author: "GHerreraE"
   },
   {
-    commit: {
-      patch: true,
-      message: "feat(htmlj): Mult exception list\n\n[222][WIP]",
-      author: {
-        name: "XCarrel",
-        date: "2025-03-20T13:43:53Z"
-      }
-    }
+    patch: true,
+    name: "2025-03-20",
+    description: "",
+    date: "2025-03-20T13:43:53Z",
+    duration: 20,
+    status: "Done",
+    author: "GHerreraE"
   },
   {
-    commit: {
-      patch: true,
-      message: "feat(htmlj): Div exception list\n\n[123][WIP]",
-      author: {
-        name: "XCarrel",
-        date: "2025-09-01T13:43:53Z"
-      }
-    }
-  },
-  {
-    commit: {
-      patch: true,
-      sha: "phonysha",
-      message: "feat(htmlj): Div exception list\n\n[123][WIP]",
-      author: {
-        name: "XCarrel",
-        date: "2025-09-01T13:43:53Z"
-      }
-    }
+    patch: true,
+    name: "2025-03-20",
+    description: "It was big",
+    date: "2025-03-20T13:43:53Z",
+    duration: 20,
+    status: "Done",
+    author: "GHerreraE"
   }
 ];
