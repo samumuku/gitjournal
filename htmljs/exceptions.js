ad8168c0e9cebbb8468c5f03e66a59be0ef893db
XCarrel <Xavier.Carrel@eduvaud.ch>
2025-09-02 12:35:06 +0200

    feat(htmljs): Save new entry to file
    By download

--- htmljs/exceptions.js
+++ htmljs/exceptions.js
@@ -1,38 +1,56 @@
 const exceptions = [
   {
-    patch: true,
-    name: "2025-09-01",
-    description: "It was bad",
-    date: "2025-09-01T13:43:53Z",
-    duration: 20,
-    status: "Done",
-    author: "GHerreraE"
+    "patch": true,
+    "name": "2025-09-01",
+    "description": "It was bad",
+    "date": "2025-09-01T13:43:53Z",
+    "duration": 20,
+    "status": "Done",
+    "author": "GHerreraE"
   },
   {
-    patch: true,
-    name: "2025-03-10",
-    description: "It was not doing much",
-    date: "2025-03-10T13:43:53Z",
-    duration: 120,
-    status: "Done",
-    author: "GHerreraE"
+    "patch": true,
+    "name": "2025-03-10",
+    "description": "It was not doing much",
+    "date": "2025-03-10T13:43:53Z",
+    "duration": 120,
+    "status": "Done",
+    "author": "GHerreraE"
   },
   {
-    patch: true,
-    name: "2025-03-20",
-    description: "",
-    date: "2025-03-20T13:43:53Z",
-    duration: 20,
-    status: "Done",
-    author: "GHerreraE"
+    "patch": true,
+    "name": "2025-03-20",
+    "description": "",
+    "date": "2025-03-20T13:43:53Z",
+    "duration": 20,
+    "status": "Done",
+    "author": "GHerreraE"
   },
   {
-    patch: true,
-    name: "2025-03-20",
-    description: "It was big",
-    date: "2025-03-20T13:43:53Z",
-    duration: 20,
-    status: "Done",
-    author: "GHerreraE"
+    "patch": true,
+    "name": "2025-03-20",
+    "description": "It was big",
+    "date": "2025-03-20T13:43:53Z",
+    "duration": 20,
+    "status": "Done",
+    "author": "GHerreraE"
+  },
+  {
+    "patch": true,
+    "name": "Hey Hey ",
+    "description": "OOooooooooHHH !!!!",
+    "date": "2025-04-01T13:43:00.000Z",
+    "duration": 25,
+    "status": "WIP",
+    "author": "GHerreraE"
+  },
+  {
+    "patch": true,
+    "name": "Schmurtzx",
+    "description": "on a fait:\n1. un\n2. deux",
+    "date": "2025-04-01T13:43:00.000Z",
+    "duration": 120,
+    "status": "Done",
+    "author": "GHerreraE"
   }
-];
+]
