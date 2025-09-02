6b49c18bcfb3a451ad047a8df90913c25ce4ff31
XCarrel <Xavier.Carrel@eduvaud.ch>
2025-09-02 08:39:39 +0200

    feat(htmljs): refactor fetch & add exceptions file

---
+++ htmljs/exceptions.js
@@ -0,0 +1,53 @@
+const exceptions = [
+  {
+    commit: {
+      patch: true,
+      message: "feat(htmlj): Add exception list\n\n[111][WIP]",
+      author: {
+        name: "XCarrel",
+        date: "2025-09-01T13:43:53Z"
+      }
+    }
+  },
+  {
+    commit: {
+      patch: true,
+      message: "feat(htmlj): Sub exception list\n\n[11][WIP]\nbla bla bla",
+      author: {
+        name: "XCarrel",
+        date: "2025-09-01T13:43:53Z"
+      }
+    }
+  },
+  {
+    commit: {
+      patch: true,
+      message: "feat(htmlj): Mult exception list\n\n[222][WIP]",
+      author: {
+        name: "XCarrel",
+        date: "2025-03-20T13:43:53Z"
+      }
+    }
+  },
+  {
+    commit: {
+      patch: true,
+      message: "feat(htmlj): Div exception list\n\n[123][WIP]",
+      author: {
+        name: "XCarrel",
+        date: "2025-09-01T13:43:53Z"
+      }
+    }
+  },
+  {
+    commit: {
+      patch: true,
+      sha: "phonysha",
+      message: "feat(htmlj): Div exception list\n\n[123][WIP]",
+      author: {
+        name: "XCarrel",
+        date: "2025-09-01T13:43:53Z"
+      }
+    }
+  }
+];
